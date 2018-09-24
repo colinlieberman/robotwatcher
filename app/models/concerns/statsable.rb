bc40420ab67080a75a2ab7f6ce21c97e3fe3b457
Colin Lieberman <colin@cactusflower.org>
2018-09-23 18:22:00 -0700

    improve stats

--- app/models/concerns/statsable.rb
+++ app/models/concerns/statsable.rb
@@ -1,13 +1,42 @@
 module Statsable
   include ActiveSupport::Concern
   def pct_uptime_since(date)
-    target = self.class.name == "Worker" ? self.worker_readings : self
     minutes_up = target.where("rate > 0").where("created_at >= '#{date}'").count.to_f
-    total_minutes = target.where("created_at > '#{date}'").count.to_f
+    total_minutes = target.where("created_at >= '#{date}'").count.to_f
     number_format((minutes_up / total_minutes) * 100)
   end
 
+  def mean_since(date)
+    number_format target.where("created_at >= '#{date}'").average(:rate)
+  end
+
+  def uptime_mean_since(date)
+    number_format target.where("rate > 0").where("created_at >= '#{date}'").average(:rate)
+  end
+
+  def mean_all_time
+    number_format target.all.average(:rate)
+  end
+
+  def mean_all_uptime
+    number_format target.where("rate > 0").average(:rate)
+  end
+
+  def max_all_time
+    number_format target.maximum("rate")
+  end
+
+  def max_since(date)
+    number_format target.where("created_at >= '#{date}'").maximum("rate")
+  end
+
+  private
+
   def number_format(val)
     val.round(2).to_f
+  end
+
+  def target
+    @@target ||= self.class.name == "Worker" ? self.worker_readings : self
   end
 end
--- app/models/pool_reading.rb
+++ app/models/pool_reading.rb
@@ -4,13 +4,5 @@ class PoolReading < ApplicationRecord
     def graph
       Grapher.day_graph "total", "Total Thps Among all Workers"
     end
-
-    def mean_all_time
-      @@mean_all_time ||= number_format PoolReading.all.average(:rate)
-    end
-
-    def mean_all_uptime
-      @@mean_all_uptime ||= number_format PoolReading.where("rate > 0").average(:rate)
-    end
   end
 end
