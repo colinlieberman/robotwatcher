f5fd5bab11a654ccdda0315dd92a17c62c7618f0
Colin Lieberman <colin@cactusflower.org>
2018-09-25 19:14:26 -0700

    get all chart data by xhr

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :worker_readings
   resources :pool_readings
   resources :workers
+
+  get 'df', to: 'pool_readings#df'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
