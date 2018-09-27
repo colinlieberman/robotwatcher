2fa0341f57d29d1b6cfe118cb3794d8d74caa6c5
Colin Lieberman <colin@cactusflower.org>
2018-09-26 18:27:49 -0700

    no active record query cacheing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :clear_active_record_query_cache
+
+  def clear_active_record_query_cache
+    ActiveRecord::Base.connection.clear_query_cache
+  end
 end
